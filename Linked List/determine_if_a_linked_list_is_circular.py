bf9a59507a9971ba3bad3d04a7f5100ff23dd13b
Raffian-moin <raffianmoin69@gmail.com>
2024-01-29 10:18:52 +0600

    Determine if a linked list is circular

---
+++ Linked List/determine_if_a_linked_list_is_circular.py
@@ -0,0 +1,43 @@
+class Node:
+    def __init__(self, data, prev=None, next=None) -> None:
+        self.data = data
+        self.prev = prev
+        self.next = next
+
+class CircularLinkedList:
+    def __init__(self, head=None) -> None:
+        self.head = head
+
+    def display(self, nodes):
+        current = nodes
+        while current:
+            print(current.data, end=" ")
+            current = current.next
+            if current == self.head:
+                break
+    
+    def determineCircularLinkedList(self):
+        current = self.head
+        while current:
+            current = current.next
+            if current == self.head:
+                return "TRUE"
+        else:
+            return "FALSE"
+
+ll = CircularLinkedList()
+node1 = Node(1)
+node2 = Node(2)
+node3 = Node(3)
+
+ll.head = node1
+
+node1.prev = node3
+node1.next = node2
+node2.prev = node1
+node2.next = node3
+node3.prev = node2
+node3.next = node1
+
+ll.display(ll.head)
+print(ll.determineCircularLinkedList())
